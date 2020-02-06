76abe0d6c835ec47a9f435191cd6b572966c65f9
GM13 <gautam.makani@gmail.com>
2020-02-06 15:06:17 -0800

    Update installCorePackages.R

--- installCorePackages.R
+++ installCorePackages.R
@@ -1,30 +1,20 @@
 # Meant to be done on a fresh R install
 
-install.packages('ggplot2')
 install.packages("tidyverse")
 install.packages("forecast")
 install.packages("plotly")
 install.packages("tidyquant")
 install.packages("timetk")
 install.packages("imputeTS")
-# install.packages("ggplotly") => not available in R 3.5.1
-install.packages("tm")
 install.packages("zoo")
 install.packages('data.table')
-install.packages('stringr')
+
 install.packages("lubridate")
-install.packages("nycflights13")
-install.packages("sweep")
 install.packages("geosphere")
 install.packages("ggcorrplot")
-install.packages("rcompanion")
 install.packages("FinCal")
-install.packages("directlabels")
 
-#install.packages("ggfortify")
-#install.packages("fUnitRoots") => messes up R installation
-# install.packages("tseries") ==> messes up R installation
-
+# install if necessary
 load <- function(pkg){
   new.pkg <- pkg[!(pkg %in% installed.packages()[, "Package"])]
   if (length(new.pkg))
